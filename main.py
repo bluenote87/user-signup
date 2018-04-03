729010eaf729e96bcd711852df339e7d7cdfd226
Paul Fannon <lurchworld@gmail.com>
2018-04-03 14:07:39 -0500

    Made a working version of the project

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, redirect, render_template
-import os
 import cgi
 import re
 
@@ -7,12 +6,12 @@ import re
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/"):
+@app.route("/")
 def index():
     return render_template('index.html')
     
-@app.route("/", methods = ['POST']):
-def index():
+@app.route("/", methods = ['POST'])
+def validate():
     un = request.form['username']
     pw = request.form['password']
     pv = request.form['verify']
@@ -23,45 +22,46 @@ def index():
     pv_error = ""
     em_error = ""
 
+
     if un == "":
         un_error = "That's not a valid username"
-    elif un.length < 3 or un.length > 20:
+    elif len(un) < 3 or len(un) > 20:
         un_error = "That's not a valid username"
 
-    un_ver = re.match(r'\s\W', un)
+    un_ver = re.search(r'\s|\W', un)
     if un_ver:
         un_error = "That's not a valid username"
 
     if pw == "":
         pw_error = "That's not a valid password"
-    elif pw.length < 3 or pw.length > 20:
+    elif len(pw) < 3 or len(pw) > 20:
         pw_error = "That's not a valid password"
     
-    pw_ver = re.match(r'\s', pw)
+    pw_ver = re.search(r'\s', pw)
     if pw_ver:
         pw_error = "That's not a valid password"
 
     if pv == "":
         pv_error = "Passwords don't match"
-    elif pv.length < 3 or pv.length > 20:
+    elif len(pv) < 3 or len(pv) > 20:
         pv_error = "Passwords don't match"
     elif pv != pw:
         pv_error = "Passwords don't match"
 
-    em_ver = re.match(r'@{1}\.+', em)
-    em_space = re.match(r'\s', em)
-        if em_space or not em_ver:
+    if em:
+        em_ver = re.search(r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b', em, re.I)
+        if not em_ver:
             em_error = "That's not a valid email"
 
     if un_error or pw_error or pv_error or em_error:
         return render_template('index.html', un_error = un_error, pw_error = pw_error,
-        pv_error = pv_error, em_error = em_error)
+        pv_error = pv_error, em_error = em_error, username = un, email = em)
     else:
         return redirect('/welcome?username={0}'.format(un))
 
 
 
-@app.route("/welcome", methods = ['GET']):
+@app.route("/welcome", methods = ['GET'])
 def welcome():
     name = request.args.get('username')
     return render_template('welcome.html', name = name)
